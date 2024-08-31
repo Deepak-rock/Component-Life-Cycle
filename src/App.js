d6da14f01b8870a395992c3a291c5db69a069fc7
Deepak <deepakind777@gmail.com>
2024-08-31 06:14:38 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,34 @@ import Clock from './components/Clock'
 import './App.css'
 
 class App extends Component {
+  state = {showClock: false}
+
+  /*   onToggleClock = () => {
+    const {showClock} = this.state
+    this.setState({showClock: !showClock})
+  }
+ */
+  onToggleClock = () => {
+    this.setState(prevState => {
+      const {showClock} = prevState
+      return {
+        showClock: !showClock,
+      }
+    })
+  }
+
   render() {
+    const {showClock} = this.state
     return (
       <div className="app-container">
-        <button type="button" className="toggle-button">
-          Hide Clock
+        <button
+          type="button"
+          className="toggle-button"
+          onClick={this.onToggleClock}
+        >
+          {showClock ? 'Hide Clock' : 'Show Clock'}
         </button>
-        <Clock />
+        {showClock && <Clock />}
       </div>
     )
   }
--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -2,10 +2,37 @@ import {Component} from 'react'
 import './index.css'
 
 class Clock extends Component {
+  // Mounting Phase
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    console.log('Constructor() Called')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount() called')
+    this.timerID = setInterval(this.tick, 1000)
+  }
+
+  // Unmounting Phase
+  componentWillUnmount() {
+    console.log('componentWillUnmount() called')
+    clearInterval(this.timerID)
+  }
+
+  // Updating State
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  // Updating Phase
   render() {
+    const {date} = this.state
+    console.log('render() called', date)
     return (
       <div className="clock-container">
         <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
       </div>
     )
   }
